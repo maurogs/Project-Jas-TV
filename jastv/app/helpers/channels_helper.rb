e5352f248921d4c223567831b8f221b39fbc072c
Mauro <mauro_gs@hotmail.com>
2014-12-10 21:12:35 +0100

    implemented episodes page

--- jastv/app/helpers/channels_helper.rb
+++ jastv/app/helpers/channels_helper.rb
@@ -3,4 +3,8 @@ module ChannelsHelper
 	def picture_for_program program 
 		program.picture || image_path('default_program_image.png')
 	end
+
+	def picture_for_channel channel
+		channel.picture || image_path('default_program_image.png')
+	end
 end
