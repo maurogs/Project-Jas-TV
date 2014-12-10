21fe085ab173cba3bf919e63ddb7077cf7508443
Mauro <mauro_gs@hotmail.com>
2014-12-10 18:49:33 +0100

    Added minimal styles to admin

--- jastv/app/controllers/site_controller.rb
+++ jastv/app/controllers/site_controller.rb
@@ -1,4 +1,8 @@
 class SiteController < ApplicationController
 	def home
 	end
+
+	def index
+		@programs = Program.all
+	end
 end
--- jastv/config/routes.rb
+++ jastv/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  get '/' => 'site#home'
 
+ get '/index' => 'site#index'
+
     resources :channels do
       resources :programs
     end
